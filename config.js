6f309d718d8508c82b8ee24625d446f7bcce54db
F4SOLO <136987422+F4SOLO@users.noreply.github.com>
2024-10-21 23:23:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB |" k",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "ZGdtYzJ_Queen-Anya_5OWg=",
 
     /**
      * thumbnails and medias
